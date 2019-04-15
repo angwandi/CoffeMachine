58c526e9187592f8e52ea7db84db173bb773f2e5
Innocent Angwadia <demsdema217@gmail.com>
2019-04-15 19:17:23 +0300

    Initial commit

---
+++ src/dems/apps/Main.java
@@ -0,0 +1,15 @@
+package dems.apps;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        String coffeeMachine = "Starting to make a coffee\n" +
+                "Grinding coffee beans\nBoiling water\n" +
+                "Mixing boiled water with crushed coffee beans\n" +
+                "Pouring coffee into the cup\n" +
+                "Pouring some milk into the cup\n" +
+                "Coffee is ready!";
+        System.out.println(coffeeMachine);
+    }
+}
