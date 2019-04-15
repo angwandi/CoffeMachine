5418e51ef2a74792e275e5c12aaaae4ce1bed540
Innocent Angwadia <demsdema217@gmail.com>
2019-04-15 19:34:28 +0300

    Stage #02

--- src/dems/apps/Main.java
+++ src/dems/apps/Main.java
@@ -1,15 +1,29 @@
 package dems.apps;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         // write your code here
-        String coffeeMachine = "Starting to make a coffee\n" +
+        Scanner scanner = new Scanner(System.in);
+       /* String coffeeMachine = "Starting to make a coffee\n" +
                 "Grinding coffee beans\nBoiling water\n" +
                 "Mixing boiled water with crushed coffee beans\n" +
                 "Pouring coffee into the cup\n" +
                 "Pouring some milk into the cup\n" +
                 "Coffee is ready!";
-        System.out.println(coffeeMachine);
+        System.out.println(coffeeMachine);*/
+//        System.out.println("Write how many cups of coffee you will need: " + numberOfCupOfCoffee);
+        System.out.println("Write how many cups of coffee you will need: ");
+        int numberOfCupOfCoffee = scanner.nextInt();
+        System.out.println("For " + numberOfCupOfCoffee + " cups of coffee you will need:");
+        int water = 200;
+        int milk = 50;
+        int beans = 15;
+
+        System.out.println(numberOfCupOfCoffee * water + " ml of water");
+        System.out.println(numberOfCupOfCoffee * milk + " ml of milk");
+        System.out.println(+numberOfCupOfCoffee * beans + " g of coffee beans");
     }
 }
